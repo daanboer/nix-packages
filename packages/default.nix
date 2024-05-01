b43d93c06ebf576e1f9393aaf3438a1024a60515
Daan Boer <daan-boer@hotmail.com>
2024-05-01 14:05:42 +0200

    Remove derivation for `ltex-ls`
    As it is now in `nixpkgs`.

--- packages/default.nix
+++ packages/default.nix
@@ -4,7 +4,6 @@ prev: final: {
   arangodb_deb = final.callPackage ./arangodb-deb { };
   g810-led = final.callPackage ./g810-led { };
   latexindent = final.callPackage ./latexindent { };
-  ltex-ls = final.callPackage ./ltex-ls { };
   pdfcrop = final.callPackage ./pdfcrop { };
   widevine = final.callPackage ./widevine { };
   tessen = final.callPackage ./tessen { };
