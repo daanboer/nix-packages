a40e65f741f34a2800a2353bfdfa2fe0c4dc9967
Daan Boer <daan-boer@hotmail.com>
2024-06-03 14:09:00 +0200

    Follow 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Nix packages flake.";
 
   inputs = {
-    stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
