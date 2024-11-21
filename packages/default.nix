d665370686cc9b30f73f194bc8e4f07d4992d6ad
Daan Boer <daan-boer@hotmail.com>
2024-11-21 11:53:45 +0100

    Remove the `tessen` package
    As it is now present in nixpkgs.

--- packages/default.nix
+++ packages/default.nix
@@ -6,22 +6,20 @@ prev: final: {
   latexindent = final.callPackage ./latexindent { };
   pdfcrop = final.callPackage ./pdfcrop { };
   widevine = final.callPackage ./widevine { };
-  tessen = final.callPackage ./tessen { };
   yp-tools = final.callPackage ./yp-tools { };
   ypbind-mt = final.callPackage ./ypbind-mt { };
   ly = final.callPackage ./ly { };
-  mendeley-reference-manager =
-    final.callPackage ./mendeley-reference-manager { };
+  mendeley-reference-manager = final.callPackage ./mendeley-reference-manager { };
 
   davmail = final.davmail.overrideAttrs (prev: rec {
     version = "6.0.1";
     src = final.fetchurl {
-      url =
-        "mirror://sourceforge/${prev.pname}/${version}/${prev.pname}-${version}-3390.zip";
+      url = "mirror://sourceforge/${prev.pname}/${version}/${prev.pname}-${version}-3390.zip";
       sha256 = "sha256-QK0G4p5QQPou67whuvzGHOgH21PL0Rb9PY8x+toMP8Q=";
     };
   });
 
-  waybar-experimental = final.unstable.waybar.overrideAttrs
-    (old: { mesonFlags = old.mesonFlags ++ [ "-Dexperimental=true" ]; });
+  waybar-experimental = final.unstable.waybar.overrideAttrs (old: {
+    mesonFlags = old.mesonFlags ++ [ "-Dexperimental=true" ];
+  });
 }
