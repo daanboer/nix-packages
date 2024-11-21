174c9607ed58eb5cb29364f5cbd90d327f09c420
Daan Boer <daan-boer@hotmail.com>
2024-11-21 11:34:27 +0100

    Switch stable branch to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Nix packages flake.";
 
   inputs = {
-    stable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
