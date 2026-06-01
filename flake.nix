0035cde383f44e7ff5c2835af412ce502e81aa37
Daan Boer <daan-boer@hotmail.com>
2026-06-01 19:31:48 +0200

    Upgrade `stable` to `26.05`

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Nixpkgs flake (stable with unstable and custom overlays).";
 
   inputs = {
-    stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # A small helper to get a standard list of systems
