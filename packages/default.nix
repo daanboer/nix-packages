43510a5aa5276d10dd179c0c6c38d30a21eae5aa
Daan Boer <daan-boer@hotmail.com>
2026-07-09 13:53:02 +0200

    Remove `davmail` derivation

--- packages/default.nix
+++ packages/default.nix
@@ -13,14 +13,6 @@ final: prev: {
 
   napi-rs-cli = prev.callPackage ./napi-rs-cli { };
 
-  davmail = prev.davmail.overrideAttrs (prev: rec {
-    version = "6.0.1";
-    src = prev.fetchurl {
-      url = "mirror://sourceforge/${prev.pname}/${version}/${prev.pname}-${version}-3390.zip";
-      sha256 = "sha256-QK0G4p5QQPou67whuvzGHOgH21PL0Rb9PY8x+toMP8Q=";
-    };
-  });
-
   waybar-experimental = prev.unstable.waybar.overrideAttrs (old: {
     mesonFlags = old.mesonFlags ++ [ "-Dexperimental=true" ];
   });
